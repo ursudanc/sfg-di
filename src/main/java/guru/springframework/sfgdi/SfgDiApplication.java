d732cd5e4ac48dd0607c02fb295e170f167eb7d4
Georgiana Dediu <Giorgiana.Dediu@extern.consorsbank.de>
2022-07-01 13:28:01 +0300

    Dependency Injection with @Qualifier

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,34 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
+import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+
+        ApplicationContext ctx  =   SpringApplication.run(SfgDiApplication.class, args);
+        MyController myCnt = (MyController) ctx.getBean("myController");
+        String greeting = myCnt.sayHello();
+        System.out.println(greeting);
+
+        System.out.println("------- Property");
+        PropertyInjectedController propertyInjC= (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+        System.out.println(propertyInjC.getGreeting());
+
+        System.out.println("------- Setter");
+        SetterInjectedController setterInjC= (SetterInjectedController) ctx.getBean("setterInjectedController");
+        System.out.println(setterInjC.getGreeting());
+
+        System.out.println("------ Constructor");
+        ConstructorInjectedController constructorInjectedC= (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+        System.out.println(constructorInjectedC.getGreeting());
 	}
 
 }
---
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PropertyInjectedController {
+
+    @Qualifier("propertyInjectedGreetingService")
+    @Autowired
+    public GreetingService greetingService;
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+
+}
